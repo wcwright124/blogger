ba9651e4b6a35aceb4a5f582e6491ef10563c718
Will <wcwright124@gmail.com>
2020-02-26 18:59:00 -0500

    fixed password reset and added to navigation

--- app/models.py
+++ app/models.py
@@ -57,6 +57,26 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def generate_reset_token(self, expiration=EXPIRATION_TIME_HOURS):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id}).decode('utf-8')
+
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+    
+    
+
 
 @login_manager.user_loader
 def load_user(user_id):
