c8151cc89df8c84698e5f92e77fbec5af6d876a9
Will <wcwright124@gmail.com>
2020-02-27 20:35:05 -0500

    added edit profile functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import flash, render_template
+from flask_login import current_user, login_required
 from . import main
+from .forms import EditProfileForm
+from ..models import User
 
 
 @main.route('/')
@@ -10,4 +13,21 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+    
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
     
