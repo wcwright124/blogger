bf6bd1e6602ad4582a2166579433f81454a14a17
Will <wcwright124@gmail.com>
2020-02-28 09:16:58 -0500

    edit profile feature for admins

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import flash, render_template
 from flask_login import current_user, login_required
 from . import main
 from .forms import EditProfileForm
+from ..decorators import admin_required
 from ..models import User
 
 
@@ -31,3 +32,29 @@ def edit_profile():
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
     
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location_data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
